89618bad33de62e04b9239d2fa811943162c8ba1
Channchealy Sek <sek.channchealy@yahoo.com>
2022-10-13 21:42:57 -0500

    save book function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -85,7 +85,7 @@ const SearchBooks = () => {
         throw new Error("something went wrong!");
       }
 
-      await // if book successfully saves to user's account, save book id to state
+      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
